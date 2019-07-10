c8d88a08600bfd858e1797f0f423e6c1303d80cb
Ari Jain <thearijain@gmail.com>
2019-07-10 19:40:46 -0400

    pushing files

---
+++ DjApplication/SamplerPopupViewController.swift
@@ -0,0 +1,46 @@
+//
+//  SamplerPopupViewController.swift
+//  DjApplication
+//
+//  Created by Anand Jain on 7/10/19.
+//  Copyright © 2019 Arihant Jain. All rights reserved.
+//
+
+import UIKit
+
+class SamplerPopupViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    //An invisble button that allows you to click anywhere outside the sampler and return back to the main storyboard
+    @IBAction func closeSamplerOverlay(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    //An invisble button that allows you to click anywhere outside the sampler and return back to the main storyboard
+    @IBAction func closeSamplerOverlay2(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    //An invisble button that allows you to click anywhere outside the sampler and return back to the main storyboard
+    @IBAction func closeSamplerOverlay3(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    //An invisble button that allows you to click anywhere outside the sampler and return back to the main storyboard
+    @IBAction func closeSamplerOverlay4(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func playChaChing(_ sender: Any) {
+        
+    }
+    
+    
+    
+    
+}
