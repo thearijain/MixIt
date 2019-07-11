c9a16d8c3c22fcde544ab07a9022317d96b8b3bd
Ari Jain <thearijain@gmail.com>
2019-07-10 21:36:20 -0400

    Almost done with the audio on the sampler.

--- DjApplication/SamplerPopupViewController.swift
+++ DjApplication/SamplerPopupViewController.swift
@@ -7,14 +7,56 @@
 //
 
 import UIKit
+import AVFoundation
 
 class SamplerPopupViewController: UIViewController {
 
+    var audioPlayer = AVAudioPlayer()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+       
     }
+    
+    //Plays the ChaChing Button
+    @IBAction func playChaChing(_ sender: Any) {
+        do {
+            audioPlayer = try AVAudioPlayer(contentsOf: URL.init(fileURLWithPath: Bundle.main.path(forResource: "ka-ching", ofType: "mp3")!))
+            audioPlayer.prepareToPlay()
+        }
+        catch {
+            print("there has been an error")
+        }
+        audioPlayer.play()
+    }
+    
+    
+    //Plays the Airhorn from the sampler
+    @IBAction func playAirHorn(_ sender: Any) {
+        do {
+            audioPlayer = try AVAudioPlayer(contentsOf: URL.init(fileURLWithPath: Bundle.main.path(forResource: "airhorn", ofType: "mp3")!))
+            audioPlayer.prepareToPlay()
+        }
+        catch {
+            print("there has been an error")
+        }
+        audioPlayer.play()
+    }
+    
+    //Plays Youu from the sampler
+    @IBAction func playYouu(_ sender: Any) {
+        do {
+            audioPlayer = try AVAudioPlayer(contentsOf: URL.init(fileURLWithPath: Bundle.main.path(forResource: "youu", ofType: "mp3")!))
+            audioPlayer.prepareToPlay()
+        }
+        catch {
+            print("there has been an error")
+        }
+        audioPlayer.play()
+    }
+    
     
     //An invisble button that allows you to click anywhere outside the sampler and return back to the main storyboard
     @IBAction func closeSamplerOverlay(_ sender: Any) {
@@ -36,9 +78,7 @@ class SamplerPopupViewController: UIViewController {
         dismiss(animated: true, completion: nil)
     }
     
-    @IBAction func playChaChing(_ sender: Any) {
-        
-    }
+    
     
     
     
