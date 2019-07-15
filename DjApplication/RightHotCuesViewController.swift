54abec841c91238ce58616dd91588dae45218809
Ari Jain <thearijain@gmail.com>
2019-07-14 22:46:57 -0400

    commit

---
+++ DjApplication/RightHotCuesViewController.swift
@@ -0,0 +1,24 @@
+//
+//  RightHotCuesViewController.swift
+//  DjApplication
+//
+//  Created by Anand Jain on 7/14/19.
+//  Copyright © 2019 Arihant Jain. All rights reserved.
+//
+
+import UIKit
+
+class RightHotCuesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    //Removes popup this is connected to four invisible buttons
+    @IBAction func closePopUp(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+}
