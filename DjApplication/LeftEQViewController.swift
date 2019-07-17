eb9dd0f990fd770d5dd0f37bc8ffbe90015123e8
Komal Shrivastava <komal@ibm.com>
2019-07-17 10:47:30 -0400

    Added EQ Pages (UI)

---
+++ DjApplication/LeftEQViewController.swift
@@ -0,0 +1,24 @@
+//
+//  LeftEQViewController.swift
+//  DjApplication
+//
+//  Created by Komal Shrivastava on 7/17/19.
+//  Copyright © 2019 Arihant Jain. All rights reserved.
+//
+
+import UIKit
+
+class LeftEQViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    //Removes popup this is connected to four invisible buttons
+    @IBAction func closePopUp(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+}
