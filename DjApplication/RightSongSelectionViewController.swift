eeca34ac7bc3b71752d25e81f39124de35a09b7a
Ari Jain <thearijain@gmail.com>
2019-07-17 13:44:51 -0400

    yyy

--- DjApplication/RightSongSelectionViewController.swift
+++ DjApplication/RightSongSelectionViewController.swift
@@ -9,14 +9,186 @@
 import UIKit
 import AVFoundation
 
-class RightSongSelectionViewController: UIViewController {
+var trackRight = AVAudioPlayer()
+var audioSessionRight = AVAudioSession.sharedInstance()
 
+
+class RightSongSelectionViewController: UIViewController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-
+    //Loads the song Forbidden Voices x Martin Garrix
+    @IBAction func loadForbiddenVoices(_ sender: Any) {
+        do {
+            trackRight = try AVAudioPlayer(contentsOf: URL.init(fileURLWithPath: Bundle.main.path(forResource: "Forbidden Voices", ofType: "mp3")!))
+            trackRight.prepareToPlay()
+            //Allows the song to play in the background
+            do {
+                try audioSessionRight.setCategory(AVAudioSession.Category.playback)
+            } catch {
+            }
+        }
+        catch {
+            print("there has been an error")
+        }
+        dismiss(animated: true, completion: nil)
+    }
+    
+    //Loads the song Secrets x Tiesto
+    @IBAction func loadSecrets(_ sender: Any) {
+        do {
+            trackRight = try AVAudioPlayer(contentsOf: URL.init(fileURLWithPath: Bundle.main.path(forResource: "Secrets", ofType: "mp3")!))
+            trackRight.prepareToPlay()
+            //Allows the song to play in the background
+            do {
+                try audioSessionRight.setCategory(AVAudioSession.Category.playback)
+            } catch {
+            }
+        }
+        catch {
+            print("there has been an error")
+        }
+        dismiss(animated: true, completion: nil)
+    }
+    
+    //Loads the song Pursuit of Happiness x Steve Aoki
+    @IBAction func loadPursuitofHappiness(_ sender: Any) {
+        do {
+            trackRight = try AVAudioPlayer(contentsOf: URL.init(fileURLWithPath: Bundle.main.path(forResource: "Pursuit of Happiness", ofType: "mp3")!))
+            trackRight.prepareToPlay()
+            //Allows the song to play in the background
+            do {
+                try audioSessionRight.setCategory(AVAudioSession.Category.playback)
+            } catch {
+            }
+        }
+        catch {
+            print("there has been an error")
+        }
+        dismiss(animated: true, completion: nil)
+    }
+    
+    //Loads the song Purple Lamborghini x Skrillex
+    @IBAction func loadPurpleLamborghini(_ sender: Any) {
+        do {
+            trackRight = try AVAudioPlayer(contentsOf: URL.init(fileURLWithPath: Bundle.main.path(forResource: "Purple Lamborghini", ofType: "mp3")!))
+            trackRight.prepareToPlay()
+            //Allows the song to play in the background
+            do {
+                try audioSessionRight.setCategory(AVAudioSession.Category.playback)
+            } catch {
+            }
+        }
+        catch {
+            print("there has been an error")
+        }
+        dismiss(animated: true, completion: nil)
+    }
+    
+    //Loads the song DJ Got Us Fallin In Love Again x Usher
+    @IBAction func loadDJGotUsFallinInLoveAgain(_ sender: Any) {
+        do {
+            trackRight = try AVAudioPlayer(contentsOf: URL.init(fileURLWithPath: Bundle.main.path(forResource: "DJ Got Us", ofType: "mp3")!))
+            trackRight.prepareToPlay()
+            //Allows the song to play in the background
+            do {
+                try audioSessionRight.setCategory(AVAudioSession.Category.playback)
+            } catch {
+            }
+        }
+        catch {
+            print("there has been an error")
+        }
+        dismiss(animated: true, completion: nil)
+    }
+    
+    //Loads Sexy Bitch x David Guetta
+    @IBAction func loadSexyBitch(_ sender: Any) {
+        do {
+            trackRight = try AVAudioPlayer(contentsOf: URL.init(fileURLWithPath: Bundle.main.path(forResource: "Sexy Bitch", ofType: "mp3")!))
+            trackRight.prepareToPlay()
+            //Allows the song to play in the background
+            do {
+                try audioSessionRight.setCategory(AVAudioSession.Category.playback)
+            } catch {
+            }
+        }
+        catch {
+            print("there has been an error")
+        }
+        dismiss(animated: true, completion: nil)
+    }
+    
+    //Loads Febreze x Jack U
+    @IBAction func loadFebreze(_ sender: Any) {
+        do {
+            trackRight = try AVAudioPlayer(contentsOf: URL.init(fileURLWithPath: Bundle.main.path(forResource: "Febreze", ofType: "mp3")!))
+            trackRight.prepareToPlay()
+            //Allows the song to play in the background
+            do {
+                try audioSessionRight.setCategory(AVAudioSession.Category.playback)
+            } catch {
+            }
+        }
+        catch {
+            print("there has been an error")
+        }
+        dismiss(animated: true, completion: nil)
+    }
+    
+    //Loads Yeah x Usher
+    @IBAction func loadYeah(_ sender: Any) {
+        do {
+            trackRight = try AVAudioPlayer(contentsOf: URL.init(fileURLWithPath: Bundle.main.path(forResource: "Yeah", ofType: "mp3")!))
+            trackRight.prepareToPlay()
+            //Allows the song to play in the background
+            do {
+                try audioSessionRight.setCategory(AVAudioSession.Category.playback)
+            } catch {
+            }
+        }
+        catch {
+            print("there has been an error")
+        }
+        dismiss(animated: true, completion: nil)
+    }
+    
+    //Loads Magenta Riddim x DJ Snake
+    @IBAction func loadMagentaRiddim(_ sender: Any) {
+        do {
+            trackRight = try AVAudioPlayer(contentsOf: URL.init(fileURLWithPath: Bundle.main.path(forResource: "Magenta Riddim", ofType: "mp3")!))
+            trackRight.prepareToPlay()
+            //Allows the song to play in the background
+            do {
+                try audioSessionRight.setCategory(AVAudioSession.Category.playback)
+            } catch {
+            }
+        }
+        catch {
+            print("there has been an error")
+        }
+        dismiss(animated: true, completion: nil)
+    }
+    
+    //Loads Get Low x Dillion Francis
+    @IBAction func loadGetLow(_ sender: Any) {
+        do {
+            trackRight = try AVAudioPlayer(contentsOf: URL.init(fileURLWithPath: Bundle.main.path(forResource: "Get Low", ofType: "mp3")!))
+            trackRight.prepareToPlay()
+            //Allows the song to play in the background
+            do {
+                try audioSessionRight.setCategory(AVAudioSession.Category.playback)
+            } catch {
+            }
+        }
+        catch {
+            print("there has been an error")
+        }
+        dismiss(animated: true, completion: nil)
+    }
     
 }
