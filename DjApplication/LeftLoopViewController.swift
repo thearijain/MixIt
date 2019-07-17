2ea22ef2e017ab99de9d0fb654b3e3af5a6e5628
Komal Shrivastava <komal@ibm.com>
2019-07-17 12:02:14 -0400

    Loop Page

---
+++ DjApplication/LeftLoopViewController.swift
@@ -0,0 +1,24 @@
+//
+//  LeftLoopViewController.swift
+//  DjApplication
+//
+//  Created by Komal Shrivastava on 7/17/19.
+//  Copyright © 2019 Arihant Jain. All rights reserved.
+//
+
+import UIKit
+
+class LeftLoopViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    //Removes popup this is connected to four invisible buttons
+    @IBAction func closePopUp(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+}
