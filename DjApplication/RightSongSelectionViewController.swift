c0dcdb94e30bfb7b43eade41c761122746b030d2
Ari Jain <thearijain@gmail.com>
2019-07-17 13:54:05 -0400

    hh

--- DjApplication/RightSongSelectionViewController.swift
+++ DjApplication/RightSongSelectionViewController.swift
@@ -42,7 +42,7 @@ class RightSongSelectionViewController: UIViewController {
     @IBAction func loadSecrets(_ sender: Any) {
         do {
             trackRight = try AVAudioPlayer(contentsOf: URL.init(fileURLWithPath: Bundle.main.path(forResource: "Secrets", ofType: "mp3")!))
-            trackRight.prepareToPlay()
+            trackRight.prepareToPlay()   
             //Allows the song to play in the background
             do {
                 try audioSessionRight.setCategory(AVAudioSession.Category.playback)
