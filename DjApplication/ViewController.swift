4506d47be043e6088bcad5100d01a5ef1ab187b4
Ari Jain <thearijain@gmail.com>
2019-07-19 22:39:32 -0400

    gxgrdg

--- DjApplication/ViewController.swift
+++ DjApplication/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
     
@@ -19,23 +20,35 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
-        //Loads right track data onto mainVC
+        //TEST
+        do {
+            trackRight = try AVAudioPlayer(contentsOf: URL.init(fileURLWithPath: Bundle.main.path(forResource: "emptyaudio", ofType: "mp3")!))
+            trackRight.prepareToPlay() }
+        catch {
+            print("error")
+        }
+        //END TEST
+        
+        //Loads right track data onto mainVC, Notification Catcher
         NotificationCenter.default.addObserver(forName: .loadRightTrackData, object: nil, queue: OperationQueue.main) { (notification) in
             let SongSelectorVC = notification.object as! RightSongSelectionViewController
             self.BPMLabel.text = BPMTrackRight
             self.TrackRightLabel.text = songNameTrackRight
             self.ImageRightLabel.image = UIImage(named: songImage)
+            
         }
     }
     
     //Plays and pauses the trackRight song
     @IBAction func playPause(_ sender: Any) {
-        if trackRight.isPlaying {
+ 
+        if (trackRight.isPlaying ) {
             trackRight.pause()
-        } else {
+            } else {
             trackRight.play()
         }
     }
+    
     
     //Restarts the trackRight song but does not play it
     @IBAction func cueButton(_ sender: Any) {
